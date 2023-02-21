f9f4ed34ab32a3367adf6e563296c921027fa203
Naseer abbas <naseer.abbas@goldevelopers.com>
2023-02-21 18:24:30 +0500

    api key added

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 const { Configuration, OpenAIApi } = require("openai");
 const fs = require('fs');
-const apikey = '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';
+const apikey = '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';
 
 const PORT = 4000;
 
